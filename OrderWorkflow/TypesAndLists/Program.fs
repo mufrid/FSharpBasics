80d06eab0d568308b05f7e884b0caf280132bfb0
Mufrid Krilic <mkr@dips.no>
2021-01-19 18:25:45 +0100

    Created solution for OrderWorkflow use case as described in 'Domain Modeling Made Functional' and started with basic list operations project. The goal is to follow the concepts from the book and try to implement a slight variation of the code examples and learn as much as possible about F#

---
+++ OrderWorkflow/TypesAndLists/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let composers = ["verdi"; "puccini"]
+let moreComposers = "vivaldi" :: composers
+let evenMoreComposers = ["paganini"]
+let allComposers = List.append moreComposers evenMoreComposers
+let printElement element = printfn "%s " element
+
+let filterEveryboydExceptForComposersStartingWithP = List.filter (fun (x:string) -> x.StartsWith("p"))
+
+[<EntryPoint>]
+let main argv =
+    printfn "Learning the basics of F# lists!"
+
+    printfn "Composers having name starting with P:"
+    allComposers 
+        |> filterEveryboydExceptForComposersStartingWithP 
+        |> List.iter printElement
+    
+    printfn "First composer in the list: %s" moreComposers.Head
+
+    0 // return an integer exit code
