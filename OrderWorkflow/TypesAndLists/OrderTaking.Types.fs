98a96d818b8db103cda10687c7b6e20483ec1f2b
mufrid <mufridk@gmail.com>
2021-02-01 09:12:40 +0100

    Added record types aka 'AND' types and a type that represent behavior, in this case type that converts payment from one currency to another

--- OrderWorkflow/TypesAndLists/OrderTaking.Types.fs
+++ OrderWorkflow/TypesAndLists/OrderTaking.Types.fs
@@ -3,3 +3,34 @@
     type OrderQuantity =
         | UnitQuantity of int
         | KilogramQuantity of decimal
+
+    type CardType = 
+        Visa | Mastercard
+
+    type CardNumber = CardNumber of string
+
+    type CreditCardInfo =
+        {
+            CardType : CardType
+            CardNumber : CardNumber
+        }
+
+    type Currency = EUR | USD
+    type PaymentAmount = PaymentAmount of decimal
+
+    type PaymentMethod =
+        | Cash
+        | Card of CardNumber
+
+
+    type Payment = 
+        {
+            Amount : PaymentAmount
+            Currency : Currency
+            Method: PaymentMethod
+        }
+
+    // Type that represent functions ie. behavior
+    // Convert payment from one currency to another
+    // First payment is input, Current is the parameter and the second payment is the output
+    type ConvertPaymentCurrency = Payment -> Currency -> Payment
